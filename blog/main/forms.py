55505efecc89231fb62496c442f67918989b373f
gevin <flyhigher139@gmail.com>
2015-05-08 00:22:09 +0800

    modify post form

--- blog/main/forms.py
+++ blog/main/forms.py
@@ -7,5 +7,5 @@ class NewPost(forms.Form):
     title = forms.CharField(max_length=256)
     content = forms.CharField(widget=forms.Textarea)
     abstract = forms.CharField(widget=forms.Textarea)
-    tag = forms.CharField(max_length=256, required=False)
-    catagory = forms.CharField(max_length=256, required=False)
+    # tag = forms.CharField(max_length=256, required=False)
+    # catagory = forms.CharField(max_length=256, required=False)
