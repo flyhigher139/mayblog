030a9a04edc4820af2197742f422aaf60923180e
gevin <flyhigher139@gmail.com>
2015-06-12 13:25:23 +0800

    add obj perm to admin

--- blog/main/admin.py
+++ blog/main/admin.py
@@ -3,11 +3,27 @@
 
 from django.contrib import admin
 from django.contrib.auth.models import Permission
+
+from guardian.admin import GuardedModelAdmin
+
 from . import models
 
 # Register your models here.
-admin.site.register(models.Post)
-admin.site.register(models.Tag)
+
+class PostAdmin(GuardedModelAdmin):
+    pass
+
+class TagAdmin(GuardedModelAdmin):
+    pass
+
+class CategoryAdmin(GuardedModelAdmin):
+    pass
+
+
+
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Tag, TagAdmin)
 admin.site.register(models.Category)
+admin.site.register(models.Page)
 admin.site.register(models.BlogMeta)
 admin.site.register(Permission)
