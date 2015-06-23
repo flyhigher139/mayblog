4e1d5cc5d67ae4cfd7c9e3a7fab21ed55005ea4f
gevin <flyhigher139@gmail.com>
2015-06-23 23:50:08 +0800

    docker content

--- blog/blog/settings/prod.py
+++ blog/blog/settings/prod.py
@@ -1,23 +1,29 @@
+import os
 from base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '111.222.333.444', 'mywebsite.com']
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db_dev.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mayblog',
-        'USER': 'root',
-        'PASSWORD': 'root_1234',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_prod.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         # 'NAME': 'mayblog',
+#         # 'USER': 'root',
+#         # 'PASSWORD': 'root_1234',
+#         # 'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         # 'PORT': '3306',
+#         'NAME': os.environ['DB_NAME'],
+#         'USER': os.environ['DB_USER'],
+#         'PASSWORD': os.environ['DB_PASS'],
+#         'HOST': os.environ['DB_SERVICE'],
+#         'PORT': os.environ['DB_PORT']
+#     }
+# }
