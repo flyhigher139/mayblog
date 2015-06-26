121e28b17c4996e9854441a6934152eec8b5907f
gevin <flyhigher139@gmail.com>
2015-06-26 21:03:33 +0800

    fix mysql connection

--- blog/blog/settings/prod.py
+++ blog/blog/settings/prod.py
@@ -5,25 +5,25 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '111.222.333.444', 'mywebsite.com']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_prod.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         # 'NAME': 'mayblog',
-#         # 'USER': 'root',
-#         # 'PASSWORD': 'root_1234',
-#         # 'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-#         # 'PORT': '3306',
-#         'NAME': os.environ['DB_NAME'],
-#         'USER': os.environ['DB_USER'],
-#         'PASSWORD': os.environ['DB_PASS'],
-#         'HOST': os.environ['DB_SERVICE'],
-#         'PORT': os.environ['DB_PORT']
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db_prod.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'mayblog',
+        # 'USER': 'root',
+        # 'PASSWORD': 'root_1234',
+        # 'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        # 'PORT': '3306',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_REMOTE_ROOT_NAME'],
+        'PASSWORD': os.environ['DB_REMOTE_ROOT_PASS'],
+        'HOST': os.environ['DB_REMOTE_ROOT_HOST'],
+        'PORT': os.environ['DB_PORT']
+    }
+}
