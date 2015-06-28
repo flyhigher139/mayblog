08e3673206d39280efa2e4f487967bfd80865f2b
gevin <flyhigher139@gmail.com>
2015-06-28 22:58:34 +0800

    modify writing layout

--- blog/main/forms.py
+++ blog/main/forms.py
@@ -5,15 +5,15 @@ from django import forms
 
 class NewPost(forms.Form):
     title = forms.CharField(max_length=256)
-    content = forms.CharField(widget=forms.Textarea)
-    abstract = forms.CharField(widget=forms.Textarea)
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows':20}))
+    abstract = forms.CharField(widget=forms.Textarea(attrs={'rows':4}))
     # tag = forms.CharField(max_length=256, required=False)
     # category = forms.CharField(max_length=256, required=False)
 
 class NewPage(forms.Form):
     title = forms.CharField(max_length=256)
     slug = forms.CharField(max_length=64)
-    content = forms.CharField(widget=forms.Textarea)
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows':20}))
 
 class CategoryForm(forms.Form):
     name = forms.CharField(max_length=256, label='Category Name')
