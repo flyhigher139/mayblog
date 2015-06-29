bf4fa6af26f6102badca2d016855fa6f48833264
gevin <flyhigher139@gmail.com>
2015-06-29 14:21:02 +0800

    fix env

--- blog/blog/settings/daocloud.py
+++ blog/blog/settings/daocloud.py
@@ -1,7 +1,7 @@
 import os
 from base import *
 
-DEBUG = os.environ['DEBUG'] or True
+DEBUG = os.environ['DEBUG'] or False
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', '111.222.333.444', 'mywebsite.com']
 
