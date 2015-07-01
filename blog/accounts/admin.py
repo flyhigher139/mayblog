6af370bcb713d151cbc767617299d383060fcf22
gevin <flyhigher139@gmail.com>
2015-07-02 00:26:26 +0800

    add accounts to user

--- blog/accounts/admin.py
+++ blog/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Account)
+admin.site.register(models.SocialInfo)
+
--- blog/accounts/models.py
+++ blog/accounts/models.py
@@ -1,3 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Account(models.Model):
+    display_name = models.CharField(max_length='128')
+    biography = models.TextField(null=True, blank=True)
+    homepage = models.URLField(null=True, blank=True)
+    weixin = models.URLField(null=True, blank=True)
+    douban = models.URLField(null=True, blank=True)
+    user = models.OneToOneField(User)
+
+    def __unicode__(self):
+        return self.display_name
+
+class SocialInfo(models.Model):
+    SOCIAL_CHOICES = (
+    ('fa-facebook', 'Facebook'),
+    ('fa-github', 'Github'),
+    ('fa-twitter', 'Twitter'),
+    ('fa-google-plus', 'Google Plus'),
+    ('fa-weibo', 'Weibo'),)
+    ('fa-bookmark', 'Other'),
+
+    user = models.ForeignKey(User)
+    social = models.CharField(choices=SOCIAL_CHOICES, max_length='128')
+    url = models.URLField()
+
+    def __unicode__(self):
+        return user.name + '-' + social
