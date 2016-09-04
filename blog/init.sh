75cf19895ab82f84bf6e5ffb038da66f0031603f
gevin <flyhigher139@gmail.com>
2016-09-04 10:45:02 +0800

    fix docker

--- blog/init.sh
+++ blog/init.sh
@@ -8,12 +8,12 @@
 # 	sleep 2
 # done
 
-if (/usr/bin/python2.7 /usr/src/app/manage.py makemigrations); then
-    if (/usr/bin/python2.7 /usr/src/app/manage.py migrate); then
-        echo "succeed to migrate database"
-        break
-    fi
-fi
+# if (/usr/bin/python2.7 /usr/src/app/manage.py makemigrations); then
+#     if (/usr/bin/python2.7 /usr/src/app/manage.py migrate); then
+#         echo "succeed to migrate database"
+#         break
+#     fi
+# fi
 
 
 /usr/local/bin/supervisord -n
